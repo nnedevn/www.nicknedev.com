aa1db1e8c01e83552f94149b10767e06e2cfa7d3
Nick Nedev <nedevn@gmail.com>
2018-01-21 23:47:17 -0800

    Changes Footer links to open in a new window

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => (
   <div className="container">
     <footer>
       <ul>
-        <li><a href="https://github.com/nnedevn"><i class="ion-social-github"></i>
+        <li><a href="https://github.com/nnedevn" target="_blank"><i class="ion-social-github"></i>
         </a></li>
-        <li><a href="https://www.linkedin.com/in/nedevn/"><i class="ion-social-linkedin"></i>
+        <li><a href="https://www.linkedin.com/in/nedevn/" target="_blank"><i class="ion-social-linkedin"></i>
         </a></li>
       </ul>
     </footer>
