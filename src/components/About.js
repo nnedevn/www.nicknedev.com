64d777c874df31ec8c38992fe5ec1b5a4713bd61
Nick Nedev <nedevn@gmail.com>
2018-01-21 21:02:13 -0800

    Adds a project to Projects Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,4 @@
 import React from 'react';
-/* Import Router */
-
 
 
 const About = () => (
@@ -10,7 +8,7 @@ const About = () => (
       <h2>About me</h2>
       <p>I am a full stack JavaScript web developer based in Seattle, Washington. I like writing clean and functional single page applications which don't break your browser's features. </p>
       <p></p>
-      <p>Here's my <a id="resume" href="assets/resume_NedevNick.pdf" target="_blank">résumé</a>.</p>
+      <p>Here's my <a id="resume" href="https://www.nicknedev.com/assets/resume_NedevNick.pdf" target="_blank">résumé</a>.</p>
     </div>
   </div>
 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,64 @@
 import React from 'react';
 /* Import Router */
-
+import emailImg from '../assets/HTMLEmail-200x150.png';
 
 
 const Projects = () => (
-  <div>Projects page</div>  
+  <div className="container site-content">
+    <div className="row">
+      <div className="col-sm-8 col-sm-offset-2">
+        <h2>Projects</h2>
+        <p className="padded"></p>
+        <article className="row">
+          <div className="col-sm-2">
+            <h4></h4>
+            <a className="red" href="http://www.nicknedev.com/sandbox/Responsive-HTML-Email-Template/"><img
+              src={emailImg} alt="" /></a>
+          </div>
+          <div className="col-sm-7 col-sm-offset-3 col-md-offset-2 red">
+            <h4>Responsive HTML Email Template</h4>
+            <p>A responsive email template constructed to render well on desktop as well as mobile email clients.
+          </p>
+            <a href="https://github.com/nnedevn/Responsive-HTML-Email-Template" target="_blank"
+              className="btn btn-primary btn-sm">View on
+            GitHub</a>
+            <a href="http://www.nicknedev.com/sandbox/Responsive-HTML-Email-Template/" target="_blank"
+              className="btn btn-primary btn-sm">View
+            Live</a>
+          </div>
+        </article>
+      </div>
+    </div>
+    <p class="padded"></p>
+
+    <div className="row">
+      <div className="col-sm-8 col-sm-offset-2">
+        
+        <p className="padded"></p>
+        <article className="row">
+          <div className="col-sm-2">
+            <h4></h4>
+            <a className="red" href="http://www.nicknedev.com/sandbox/Responsive-HTML-Email-Template/"><img
+              src={emailImg} alt="" /></a>
+          </div>
+          <div className="col-sm-7 col-sm-offset-3 col-md-offset-2 red">
+            <h4>Responsive HTML Email Template</h4>
+            <p>A responsive email template constructed to render well on desktop as well as mobile email clients.
+          </p>
+            <a href="https://github.com/nnedevn/Responsive-HTML-Email-Template" target="_blank"
+              className="btn btn-primary btn-sm">View on
+            GitHub</a>
+            <a href="http://www.nicknedev.com/sandbox/Responsive-HTML-Email-Template/" target="_blank"
+              className="btn btn-primary btn-sm">View
+            Live</a>
+          </div>
+        </article>
+      </div>
+    </div>
+
+  </div>
+
+
 );
 
 export default Projects
